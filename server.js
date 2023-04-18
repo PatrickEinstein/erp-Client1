2b93be06a8c86abe1047fd5eda82a59339206170
PatrickEinstein <mohammedola1234@gmail.com>
2023-04-18 10:17:52 -0400

    add headers

--- server.js
+++ server.js
@@ -56,11 +56,16 @@ app.use(express.static(path.join(__dirname, "public/")));
 app.use(logger);
 
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
+// Allow cross-origin requests from all domains
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, application/json');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
 
 const connectDB = (url) => {
