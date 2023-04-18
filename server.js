3eb7ef41ca5dbc6e5e4f7a159f4a490c007d3514
PatrickEinstein <mohammedola1234@gmail.com>
2023-04-18 09:46:58 -0400

    double header

--- server.js
+++ server.js
@@ -46,7 +46,11 @@ const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const port = process.env.PORT || 5001;
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:3000',
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}))
 app.use(helmet.crossOriginResourcePolicy({policy:"cross-origin"}));
 app.use(xss());
 app.use(mongoSanitize());
