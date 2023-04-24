52c7b791b91a7c1ef8b04204ea4561f1fc5da70d
PatrickEinstein <mohammedola1234@gmail.com>
2023-04-24 17:03:16 -0400

    combined both repos to proxyway

--- server.js
+++ server.js
@@ -56,23 +56,7 @@ app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
-// app.use(cors({
-//   origin:"http://localhost:3000",
-//   credentials:true
-// }))
 
-const whitelist = ["http://localhost:3000"]
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true)
-    } else {
-      callback(new Error("Not allowed by CORS"))
-    }
-  },
-  credentials: true,
-}
-app.use(cors(corsOptions))
 
 
 
