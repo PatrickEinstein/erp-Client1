66cd76103e253ee633184a07c793786aca6a4c64
PatrickEinstein <mohammedola1234@gmail.com>
2023-04-25 08:56:06 -0400

    removed phantom

--- model/user.js
+++ model/user.js
@@ -23,7 +23,7 @@ const UserSchema = new mongoose.Schema({
             validator: validator.isEmail,
             message: 'Please provide a valid email'
         },
-        // unique: true,
+    
     },
     phone: {
         type: Number,
