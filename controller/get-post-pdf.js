835031e69d51f9f0973cffec12408baac1b42d8a
Sayrikey1 <kamalseriki49@gmail.com>
2023-04-30 21:14:54 +0100

    pdf

--- controller/get-post-pdf.js
+++ controller/get-post-pdf.js
@@ -3,25 +3,25 @@ import fs from "fs";
 const getPdf = (req, res) => {
 
   const filePath = req.params.path;
-  fs.readFileSync(filePath, (err, data) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.send(data);
-    }
-  });
+  const pdfBuffer = fs.readFileSync("download.pdf", filePath);
+  res.send(pdfBuffer)
+  
 };
-const Post = (req, res) => {
+
+// const Post = (req, res) => {
     
-    const filePath = req.params.path;
-    fs.writeFileSync(filePath, (err, data) => {
-      if (err) {
-        res.status(500).send(err);
-      } else {
-        res.send(data);
-      }
-    });
-  };
+//     const filePath = req.params.path;
+//     const pdfBuffer = fs.readFileSync("download.pdf");
+//     // fs.writeFileSync(filePath, (err, data) => {
+//     //   if (err) {
+//     //     res.status(500).send(err);
+//     //   } else {
+//     //     res.send(data);
+//     //   }
+//     // });
+
+//     res.send(pdfBuffer)
+//   };
 
 
 export default getPdf;
